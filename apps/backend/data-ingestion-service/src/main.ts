3540cbf7e54fa57dcff2761dba39724ddf63e0e5
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-01-12 02:08:24 -0500

    fix: enable cors in ingestion service to resolve failed to fetch error

--- apps/backend/data-ingestion-service/src/main.ts
+++ apps/backend/data-ingestion-service/src/main.ts
@@ -9,12 +9,18 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // 1. Habilitar CORS para permitir peticiones desde el frontend (puerto 4200)
+  app.enableCors(); 
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  
   const port = process.env.PORT || 3000;
   await app.listen(port);
+  
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 ETL Ingestion Service is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
