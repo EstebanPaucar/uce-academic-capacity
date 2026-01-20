599186a90268d1372ff38ebaf1a41d4bccb09fc7
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-01-20 13:56:23 -0500

    feat(audit): implement audit-service with MongoDB and Mongoose for flexible activity logging

--- apps/backend/audit-service/src/app/app.module.ts
+++ apps/backend/audit-service/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuditLog, AuditLogSchema } from './schemas/audit-log.schema';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // Conexión a Mongo (Asegúrate de que la URL sea correcta para AWS/Docker)
+    MongooseModule.forRoot(process.env.MONGO_URL || 'mongodb://localhost:27017/uce_audit_db'),
+    MongooseModule.forFeature([{ name: AuditLog.name, schema: AuditLogSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
