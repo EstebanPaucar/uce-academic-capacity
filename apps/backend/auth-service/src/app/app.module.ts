cddbb7694bccd6324c970dfb5f6f9db977c80adc
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-01-20 12:49:19 -0500

    feat(auth): configure JwtModule and secret keys for auth-service

--- apps/backend/auth-service/src/app/app.module.ts
+++ apps/backend/auth-service/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    PassportModule,
+    // Configuración del JWT
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'uce_secret_key_2026', // Usa una variable de entorno
+      signOptions: { expiresIn: '8h' }, // El token dura una jornada laboral
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AuthService],
 })
 export class AppModule {}
