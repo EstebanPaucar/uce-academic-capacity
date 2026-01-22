05c6a9f7df6cb5432cfb06f3868b5bd0aaefcdc2
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-01-21 20:36:01 -0500

    feat(notification-service): add app module configuration

--- apps/backend/notification-service/src/app/app.module.ts
+++ apps/backend/notification-service/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({ secret: 'SECRET_KEY_UCE_2026', signOptions: { expiresIn: '1h' }, }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
