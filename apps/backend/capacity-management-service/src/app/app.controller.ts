2f3770e57a342ff667e971838834f0eba4782787
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-01-22 00:07:03 -0500

    feat(capacity-management): add capacity controller endpoints

--- apps/backend/capacity-management-service/src/app/app.controller.ts
+++ apps/backend/capacity-management-service/src/app/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload, Ctx, RmqContext } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @EventPattern('execute_capacity_change')
+  async handleExecution(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const msg = context.getMessage();
+    
+    await this.appService.executeChange(data);
+    
+    channel.ack(msg);
   }
 }
