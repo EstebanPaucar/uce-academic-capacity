a555ae8f8458e6d2fd4a41ee4c721126f6e1a563
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-01-21 21:05:25 -0500

    feat(analytics-service): add analytics controller endpoints

--- apps/backend/analytics-service/src/app/app.controller.ts
+++ apps/backend/analytics-service/src/app/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('analytics')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData() {
-    return this.appService.getData();
+  async getStats(@Query('role') role: string, @Query('facultyId') facultyId: string) {
+    return this.appService.getAnalytics(role, Number(facultyId));
   }
 }
