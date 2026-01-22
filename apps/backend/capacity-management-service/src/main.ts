eda2e5c92c32cece7b3e3074efec1e3138fa66b6
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-01-22 00:07:09 -0500

    chore(capacity-management): configure service bootstrap

--- apps/backend/capacity-management-service/src/main.ts
+++ apps/backend/capacity-management-service/src/main.ts
@@ -1,22 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3009;
-  app.enableCors();
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
+  
+  // Conexión para escuchar órdenes del Request Service
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost:5672'],
+      queue: 'capacity_execution_queue',
+      noAck: false,
+      queueOptions: { durable: true },
+    },
+  });
 
+  await app.startAllMicroservices();
+  await app.listen(3009); 
+}
 bootstrap();
