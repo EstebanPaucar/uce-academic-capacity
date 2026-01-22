9c1a562efad74666827c18c99057f93e089af980
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-01-22 00:06:52 -0500

    feat(capacity-management): add module configuration

--- apps/backend/capacity-management-service/src/app/app.module.ts
+++ apps/backend/capacity-management-service/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'GO_ENGINE', // Cliente para hablar con Go
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost:5672'],
+          queue: 'academic_data_queue', // La misma cola de ingesta
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
