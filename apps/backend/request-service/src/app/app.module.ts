845499f30d8be7f39fa99668f037a7abb0b4b8f3
EstebanPaucar <tebanpaucar04@hotmail.com>
2026-01-22 00:02:05 -0500

    feat(request-service): add module configuration

--- apps/backend/request-service/src/app/app.module.ts
+++ apps/backend/request-service/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'CAPACITY_MQ', // Conectamos con el servicio ejecutor
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost:5672'],
+          queue: 'capacity_execution_queue', // Cola de ejecución
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
